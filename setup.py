7430ef30c24b099ce7832cd5c95573aae83f981b
Christopher Auyeung <chris@winfieldco.com>
2016-03-31 13:59:03 -0400

    Updated post version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst") as f:
 
 setup(
     name = "yweather",
-    version = "0.1-post2",
+    version = "0.1-post3",
     author = "Thomas Roten",
     author_email = "thomas@roten.us",
     url = "https://github.com/tsroten/yweather",
